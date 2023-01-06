b38f2683c6bd661f1498d0ef2a513489f870b84d
Brentably <Brent@pabnet.com>
2023-01-05 21:49:42 -0500

    finished basic template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Web3OnboardProvider, init } from '@web3-onboard/react'
+import injectedModule from '@web3-onboard/injected-wallets'
+import walletConnectModule from '@web3-onboard/walletconnect'
+
+
+const rpcUrl = `https://eth-goerli.g.alchemy.com/v2/qNtE2MdnnNXNh8G5hjIZ-baxFqFnqvoQ`
+
+const injected = injectedModule()
+const walletConnect = walletConnectModule()
+const web3Onboard = init({
+  wallets: [injected, walletConnect],
+  chains: [
+    {
+      id: 80001,
+      token: 'MATIC',
+      label: 'Polygon Mumbai',
+      rpcUrl
+    }
+  ]
+})
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return ( 
+  <Web3OnboardProvider web3Onboard={web3Onboard}>
+    <Component {...pageProps} /> 
+  </Web3OnboardProvider>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,29 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { useConnectWallet } from '@web3-onboard/react'
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet()
+  const [address, setAddress] = useState<string>('')
+  const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>(null)
+  const [signer, setSigner] = useState<ethers.Signer | null>(null)
+
+  // updates address, provider, signer based on web3Onboard's wallet
+  useEffect(() => {
+    setAddress(wallet?.accounts[0].address || "")
+    const provider = wallet ? new ethers.providers.Web3Provider(wallet.provider, 'any') : null
+    provider ? setProvider(provider) : console.log('ooops, couldnt get provider. provider is', provider)
+    const signer = provider?.getSigner() 
+    signer ? setSigner(signer) : console.log('oops, couldnt get signer, signer is', signer)
+    console.log('address, provider, signer connected')
+  }, [wallet])
+
+
   return (
     <>
       <Head>
@@ -14,110 +33,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <div className='text-center'>
+        <h1 className='text-3xl'>App</h1>
+        <div className='text-xl'>Welcome to the app</div>
+        <button type="button" className='bg-slate-400 rounded-xl p-2 mt-1' disabled={connecting} onClick={() => (wallet ? disconnect(wallet) : connect())}> {connecting ? 'connecting' : wallet ? 'disconnect' : 'connect'} </button>
+      </div>
     </>
   )
 }
